139c6ca45ebd8a7854ea642a95d5a495e1ae2589
Sachin Lakshitha <niroshasulochini@gmail.com>
2026-02-11 21:18:56 +0530

    feat: implement DTO fields for request and response classes

--- src/main/java/com/bookfair/dto/request/RegisterRequest.java
+++ src/main/java/com/bookfair/dto/request/RegisterRequest.java
@@ -1,4 +1,12 @@
 package com.bookfair.dto.request;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String businessName;
+    private String email;
+    private String contactPerson;
+    private String password;
+    private String phone;
 }
