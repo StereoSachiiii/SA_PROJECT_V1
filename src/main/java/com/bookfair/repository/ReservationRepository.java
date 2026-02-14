43f62ec1d0fce04c80f73910900111c86793d3ca
Nihadiyan <nihath854@gmail.com>
2026-02-14 20:35:01 +0530

    resolve IDE warnings using @Override and @NonNull for List in repo

--- src/main/java/com/bookfair/repository/ReservationRepository.java
+++ src/main/java/com/bookfair/repository/ReservationRepository.java
@@ -5,12 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.lang.NonNull;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @org.springframework.data.jpa.repository.Query("SELECT r FROM Reservation r JOIN FETCH r.stall JOIN FETCH r.user WHERE r.user.id = :userId")
     List<Reservation> findByUserId(Long userId);
 
+    @Override
+    @NonNull
     @org.springframework.data.jpa.repository.Query("SELECT r FROM Reservation r JOIN FETCH r.stall JOIN FETCH r.user")
     List<Reservation> findAll();
     
