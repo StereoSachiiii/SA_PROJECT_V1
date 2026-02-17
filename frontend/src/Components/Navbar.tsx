d4f8e6c02370dbaef875cae67b9a84e6a6ae8693
Nnavodya <rajapakse22030.kln@gmail.com>
2026-02-17 12:39:07 +0530

    Umdated the font size of nav bar and change the color

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -10,13 +10,13 @@ function Navbar() {
             className="bg-blue-800 shadow"
             variant="dark"
         >
-            <Container fluid className="px-4">
-                
-                {/* Logo Section */}
+            <Container fluid className="px-3">
+
+                {/* Logo */}
                 <BootstrapNavbar.Brand
                     as={Link}
                     to="/home"
-                    className="flex items-center gap-2 text-4xl font-extrabold tracking-wide m-0"
+                    className="d-flex align-items-center gap-2 text-4xl font-extrabold tracking-wide m-0"
                 >
                     <FaBookOpen className="text-yellow-300 text-3xl" />
                     <span className="text-white">
@@ -27,23 +27,41 @@ function Navbar() {
                 <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
 
                 <BootstrapNavbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto ms-4">
-                        <Nav.Link as={Link} to="/home">
+
+                    {/* LEFT SIDE LINKS */}
+                    <Nav className="me-auto">
+
+                        <Nav.Link
+                            as={Link}
+                            to="/home"
+                            className="text-white fs-5 fw-semibold me-4"
+                        >
                             Home
                         </Nav.Link>
 
-                        <Nav.Link as={Link} to="/stalls">
+                        <Nav.Link
+                            as={Link}
+                            to="/stalls"
+                            className="text-white fs-5 fw-semibold me-4"
+                        >
                             Stalls
                         </Nav.Link>
 
-                        <Nav.Link as={Link} to="/employee">
+                        <Nav.Link
+                            as={Link}
+                            to="/employee"
+                            className="text-white fs-5 fw-semibold"
+                        >
                             Employee Portal
                         </Nav.Link>
+
                     </Nav>
 
+                    {/* RIGHT SIDE BUTTON */}
                     <Button variant="outline-light">
                         Logout
                     </Button>
+
                 </BootstrapNavbar.Collapse>
 
             </Container>
