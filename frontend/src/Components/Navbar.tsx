4bbed65c5eae40bbc8affb314613c49498a7218d
Nnavodya <rajapakse22030.kln@gmail.com>
2026-02-17 11:45:53 +0530

    Left alligned the website name BookfairZone

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -10,14 +10,15 @@ function Navbar() {
             className="bg-blue-800 shadow"
             variant="dark"
         >
-            <Container>
+            <Container fluid className="px-4">
+                
                 {/* Logo Section */}
                 <BootstrapNavbar.Brand
                     as={Link}
                     to="/home"
-                    className="flex items-center gap-2 text-2xl font-extrabold tracking-wide"
+                    className="flex items-center gap-2 text-4xl font-extrabold tracking-wide m-0"
                 >
-                    <FaBookOpen className="text-yellow-300 text-2xl" />
+                    <FaBookOpen className="text-yellow-300 text-3xl" />
                     <span className="text-white">
                         Bookfair<span className="text-yellow-300">Zone</span>
                     </span>
@@ -26,7 +27,7 @@ function Navbar() {
                 <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
 
                 <BootstrapNavbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="me-auto ms-4">
                         <Nav.Link as={Link} to="/home">
                             Home
                         </Nav.Link>
@@ -44,9 +45,10 @@ function Navbar() {
                         Logout
                     </Button>
                 </BootstrapNavbar.Collapse>
+
             </Container>
         </BootstrapNavbar>
     )
 }
 
-export default Navbar;
+export default Navbar
